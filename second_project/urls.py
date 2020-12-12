589dffc8d8f57a59d6ed572f4e4fd9b473a0fa8c
Abandan007 <75872539+Abandan007@users.noreply.github.com>
2020-12-12 18:35:08 +0530

    new

--- second_project/urls.py
+++ second_project/urls.py
@@ -23,6 +23,4 @@ from django.conf.urls import url
 urlpatterns = [
     path('',views.index,name='index'),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
